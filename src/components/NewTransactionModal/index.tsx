be24708e2054a7d6f4cb32b0ea21c1411bf2c7df
gustavo falcione <falcionegustavo@gmail.com>
2022-04-26 11:05:50 -0300

    Estruturando formulario e estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
+import closeImg from '../../assets/close.svg';
+
 import Modal from 'react-modal'
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +13,38 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
     <Modal 
         isOpen={isOpen} 
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+    >
+      <button 
+        type='button' 
+        onClick={onRequestClose}
+        className="react-modal-close"
       >
+        <img src={closeImg} alt="fechar modal" />
+      </button> 
+
+      <Container>
         <h2>Cadastrar transação</h2>
-      </Modal>
+
+        <input 
+        placeholder='Título'   
+        />
+
+        <input 
+          placeholder='Valor' 
+          type="number"
+        />
+
+        <input 
+          placeholder='Categoria' 
+        />
+
+        <button type='submit'>
+          Cadastrar
+        </button>
+      </Container> 
+
+    </Modal>
   )
 }
