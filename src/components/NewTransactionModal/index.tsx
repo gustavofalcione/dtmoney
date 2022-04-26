52913d6e63d208d68f04ca825e78a36dae43f791
gustavo falcione <falcionegustavo@gmail.com>
2022-04-26 15:35:36 -0300

    corrigindo detalhes a insercao de dados na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { api } from '../../services/api';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
@@ -22,12 +23,14 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
 
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault()
-    console.log({
+    const data = {
       title,
       category,
       value,
       type,
-    })
+    }
+
+    api.post('/transactions', data)
   }
 
   return(
